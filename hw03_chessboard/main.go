9edcfc2cdeacd49e01efbf254abcce7ad6df6bdb
Aleksandr Torzhenov <shiskro@iMac-Aleksandr.local>
2024-12-17 18:37:04 +0300

    hw03 chessboard

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	size := 8 // Размер доски (N x N).
+	board := createChessBoard(size)
+	fmt.Println(board)
+}
+
+func createChessBoard(size int) string {
+	var board string
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if (i+j)%2 == 0 {
+				board += "#"
+			} else {
+				board += " "
+			}
+		}
+		board += "\n"
+	}
+	return board
 }
